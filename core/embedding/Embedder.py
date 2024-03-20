80cfdd03b692e4bb5541145983e7c0b136bc4753
panzer0 <daniel.pazur@gmail.com>
2024-03-20 20:16:26 +0100

    Read data via pandas library

---
+++ core/embedding/Embedder.py
@@ -0,0 +1,16 @@
+import pandas
+
+DATASET_FILENAME = "dataset_privacy.json"
+DATASET_PATH = f"datasets/{DATASET_FILENAME}"
+
+class Embedder:
+    def __init__(self, dataset_path) -> None:
+        self.dataset = self.load_dataset(dataset_path)
+
+    def load_dataset(self, path):
+        return pandas.read_json(path)
+
+
+if __name__ == '__main__':
+    embedder = Embedder(DATASET_PATH)
+    print(embedder.dataset)
