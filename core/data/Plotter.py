f94a2cf7ea46e74260dbbb79e543dff1e2e00e07
panzer0 <daniel.pazur@gmail.com>
2024-04-11 15:37:50 +0200

    Add global const comments

--- core/data/Plotter.py
+++ core/data/Plotter.py
@@ -3,9 +3,11 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 
-
+# Name of the subreddit the default dataset is derived from
 SUBREDDIT_NAME = "fantasy"
+# The default dataset's filename
 DATASET_FILENAME = f"dataset_{SUBREDDIT_NAME}.parquet.gzip"
+# The default dataset's path
 DATASET_PATH = f"datasets/{DATASET_FILENAME}"
 
 
