4c72bf39a397baaa8515f059abca4bb103509b2e
panzer0 <daniel.pazur@gmail.com>
2024-04-16 16:49:11 +0200

    Add balance_dataset() docstring

--- core/data/Preprocessing.py
+++ core/data/Preprocessing.py
@@ -7,12 +7,28 @@ DATASET_FILENAME = f"dataset_{SUBREDDIT_NAME}.parquet.gzip"
 # The default dataset's path
 DATASET_PATH = f"datasets/{DATASET_FILENAME}"
 
-#
-def balance_dataset(df: pd.DataFrame) -> pd.DataFrame:
+
+def balance_dataset(df: pd.DataFrame, sample_count: int) -> pd.DataFrame:
+    """Balances a dataset.
+
+    Returns a balanced version of the provided data. A balanced dataset
+    is one that contains an equal amount of samples belonging to each class
+    - in this case the authorship. This is achieved by omitting the classes
+    that have fewer samples than the desired amount, then randomly sampling
+    the same amount from the remaining ones.
+
+    Args:
+        df: The dataframe that is to be balanced.
+        sample_count: The desired amount of samples of each class in the
+         balanced dataset.
+
+    Returns:
+        A balanced version of the given dataframe.
+    """
     return (
-        df.groupby('author')
+        df.groupby("author")
             .filter(lambda x: len(x) >= 50)
-            .groupby('author')
+            .groupby("author")
             .apply(lambda x: x.sample(n=50, random_state=42))
             .reset_index(level=0, drop=True)
     )
