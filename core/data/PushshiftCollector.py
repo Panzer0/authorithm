8ea748939ac02ddf6c994f6c66f971e4cd2700a9
panzer0 <daniel.pazur@gmail.com>
2024-07-26 18:07:03 +0200

    Implement PushshiftCollector class

---
+++ core/data/PushshiftCollector.py
@@ -0,0 +1,136 @@
+import json
+import pandas as pd
+from core.data.Embedder import Embedder
+import pyarrow as pa
+import pyarrow.parquet as pq
+from tqdm import tqdm
+import zstandard as zstd
+import io
+from core.config import UNCOMPRESSED_PATH, COMPRESSED_PATH
+
+# Name of the subreddit the default dataset is derived from
+SUBREDDIT_NAME = "fantasy"
+
+
+class ParquetChunkWriter:
+    def __init__(self, parquet_file_path: str, compression: str):
+        self.parquet_file_path = parquet_file_path
+        self.compression = compression
+        self.writer = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.writer is not None:
+            self.writer.close()
+
+    def write_chunk(self, data_chunk):
+        df = pd.DataFrame(data_chunk)
+        table = pa.Table.from_pandas(df)
+
+        if self.writer is None:
+            self.writer = pq.ParquetWriter(
+                self.parquet_file_path,
+                table.schema,
+                compression=self.compression,
+                use_dictionary=False,
+                write_statistics=False,
+            )
+
+        self.writer.write_table(table)
+
+
+class PushshiftCollector:
+    def __init__(self) -> None:
+        self.embedder = Embedder()
+        self.dataset = None
+
+    def zst_to_parquet_with_embeddings(
+        self,
+        zst_file_path: str,
+        parquet_file_path: str,
+        chunksize: int = 100000,
+        compression: str = "gzip",
+    ):
+        try:
+            total_lines = self._count_lines(zst_file_path)
+            print(f"Total lines in file: {total_lines}")
+
+            with self._open_zst_file(
+                zst_file_path
+            ) as text_stream, ParquetChunkWriter(
+                parquet_file_path, compression
+            ) as writer:
+                self._process_stream(
+                    text_stream, writer, chunksize, total_lines
+                )
+
+            print(
+                f"\nConversion complete. Output saved to {parquet_file_path} with {compression} compression."
+            )
+        except FileNotFoundError:
+            print(f"Error: The file {zst_file_path} was not found.")
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
+    def _count_lines(self, zst_file_path: str) -> int:
+        total_lines = 0
+        with self._open_zst_file(zst_file_path) as text_stream:
+            for _ in tqdm(text_stream, desc="Counting lines", unit=" lines"):
+                total_lines += 1
+        return total_lines
+
+    def _process_stream(
+        self, text_stream, writer, chunksize: int, total_lines: int
+    ):
+        data_chunks = []
+        chunk_count = 0
+
+        with tqdm(
+            total=total_lines, desc="Processing lines", unit="line"
+        ) as pbar:
+            for line in text_stream:
+                try:
+                    data = self._process_line(line)
+                    data_chunks.append(data)
+                    if len(data_chunks) == chunksize:
+                        writer.write_chunk(data_chunks)
+                        data_chunks.clear()
+                        chunk_count += 1
+                    pbar.update(1)
+                    pbar.set_postfix({"Chunks": chunk_count})
+                except json.JSONDecodeError:
+                    pbar.write(
+                        f"Warning: Skipping invalid JSON on line {pbar.n + 1}"
+                    )
+
+        if data_chunks:
+            writer.write_chunk(data_chunks)
+
+    def _open_zst_file(self, file_path):
+        fh = open(file_path, "rb")
+        dctx = zstd.ZstdDecompressor()
+        stream_reader = dctx.stream_reader(fh)
+        return io.TextIOWrapper(stream_reader, encoding="utf-8")
+
+    def _process_line(self, line):
+        data = json.loads(line)
+        body = data.get("body", "")
+        embedding = self.embedder.embed_str(body)
+        embedding_dict = self.embedder.to_dict(embedding)
+        word_count = len(body.split())
+
+        filtered_data = {
+            "id": data.get("id"),
+            "author": data.get("author"),
+            "body": body,
+            "word_count": word_count,
+        }
+        filtered_data.update(embedding_dict)
+        return filtered_data
+
+
+if __name__ == "__main__":
+    collector = PushshiftCollector()
+    collector.zst_to_parquet_with_embeddings(COMPRESSED_PATH, UNCOMPRESSED_PATH)
