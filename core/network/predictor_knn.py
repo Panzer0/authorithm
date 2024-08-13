9b1630681b76e55dbc9cf36a8a4b88be307b5499
panzer0 <daniel.pazur@gmail.com>
2024-08-13 17:48:00 +0200

    Convert predictor_knn to work on PCADataset

--- core/network/predictor_knn.py
+++ core/network/predictor_knn.py
@@ -1,8 +1,8 @@
-from core.data.reddit_dataset import RedditDataset
+from core.data.pca_dataset import PCADataset
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 
-from core.config import DATASET_PATH
+from core.config import DATASET_PCA_PATH
 
 
 class PredictorKNN:
@@ -11,19 +11,19 @@ class PredictorKNN:
 
 
 if __name__ == "__main__":
-    dataset = RedditDataset(DATASET_PATH)
-    embed_train, embed_test, authors_train, authors_test = train_test_split(
-        dataset.embeddings,
+    dataset = PCADataset(DATASET_PCA_PATH)
+    pca_train, pca_test, authors_train, authors_test = train_test_split(
+        dataset.pca,
         dataset.authors,
         stratify=dataset.authors,
         random_state=42,
         test_size=0.2,
     )
 
-    k = 5
+    k = 50
     knn = KNeighborsClassifier(n_neighbors=k)
-    knn.fit(embed_train, authors_train)
+    knn.fit(pca_train, authors_train)
 
-    y_pred = knn.predict(embed_test)
-    accuracy = knn.score(embed_test, authors_test)
+    y_pred = knn.predict(pca_test)
+    accuracy = knn.score(pca_test, authors_test)
     print(f"Accuracy: {accuracy}")
