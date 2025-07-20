681f0efc2c78fc927a00ba0339405dbca4d0cc2b
panzer0 <daniel.pazur@gmail.com>
2025-07-20 11:37:53 +0200

    Extract parquet_chunk_writer.py to separate file

--- core/data/processors/pushshift_embedding_processor.py
+++ core/data/processors/pushshift_embedding_processor.py
@@ -4,57 +4,7 @@ from tqdm import tqdm
 import zstandard as zstd
 import io
 from core.config import UNCOMPRESSED_PATH, COMPRESSED_PATH
-
-
-class ParquetChunkWriter:
-    """Writes to a parquet file in chunks.
-
-    Attributes:
-        parquet_file_path: The path of the parquet file to be written to.
-        compression: The compression type of the parquet file.
-        writer: The writer to write the parquet file.
-    """
-
-    def __init__(self, parquet_file_path: str, compression: str):
-        """Inits ParquetChunkWriter.
-
-        Args:
-            parquet_file_path: The path of the parquet file to be written to.
-            compression: The compression type of the parquet file.
-        """
-        self.parquet_file_path = parquet_file_path
-        self.compression = compression
-        self.writer = None
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        if self.writer is not None:
-            self.writer.close()
-
-    def write_chunk(self, data_chunk) -> None:
-        """Writes a single data chunk
-
-        Writes a single data chunk to the parquet file, compressing it with the
-        specified compression type.
-
-        Args:
-            data_chunk: The data chunk to be written.
-        """
-        df = pd.DataFrame(data_chunk)
-        table = pa.Table.from_pandas(df)
-
-        if self.writer is None:
-            self.writer = pq.ParquetWriter(
-                self.parquet_file_path,
-                table.schema,
-                compression=self.compression,
-                use_dictionary=False,
-                write_statistics=False,
-            )
-
-        self.writer.write_table(table)
+from core.data.processors.parquet_chunk_writer import ParquetChunkWriter
 
 
 class PushshiftEmbeddingProcessor:
